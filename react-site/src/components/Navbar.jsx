59ccd9779a41b0de8a35adf0127e0236b49f090c
Priyanka <pbhaukajee@gmail.com>
2024-07-17 14:49:02 -0500

    complete nav section

---
+++ react-site/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import Logo from "../assets/images/react-icon-small.png";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <div className="nav-left">
+        <img className="nav--icon" src={Logo} alt="React-Logo" />
+        <h3 className="nav--logo_text">ReactFacts</h3>
+      </div>
+
+      <h4 className="nav--title">React Course - Project 1</h4>
+    </nav>
+  );
+}
